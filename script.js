d0d25f61e1dfaa7a7995f089cfb16daacaa00ffd
akhilbns <74220096+akhil12345-cloud@users.noreply.github.com>
2026-02-10 10:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+let cost = 0;
 
-module.exports=mincost;
+function sortedArr(unSortedArray) {
+  unSortedArray.sort((a, b) => {
+    return a - b;
+  });
+
+  minCost(unSortedArray);
+}
+sortedArr(arr);
+
+function minCost(arr) {
+  if (arr.length > 1) {
+    let minimum = arr[0] + arr[1];
+    cost += minimum;
+    arr.shift()
+    arr.shift()
+
+    arr.push(minimum);
+    sortedArr(arr);
+  } else {
+    return cost;
+  }
+}
+console.log(minCost(arr));
